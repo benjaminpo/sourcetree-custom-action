aa6c1d2b7786193e938e89b8426c41368761a49a
Benjamin Po <benjaminpo212@gmail.com>
2019-02-27 11:36:07 +0800

    updating git-cleanup

--- bin/git-cleanup.sh
+++ bin/git-cleanup.sh
@@ -1,12 +1,14 @@
 #!/bin/sh
 
 # File: git-cleanup.sh
-# Parameters: $SHA
+# Parameters: N/A
 #
 # Note: Required manual refresh (command+r) after perform custom action
 
 
+
 # Core
 
+git fetch --tags -f
 git gc --prune=now
 git remote prune origin
